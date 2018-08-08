9ec1d1ff84e7a684ccf3279ec1f29e6fce3c201e
k-burn <karaburnett93@gmail.com>
2018-08-08 13:59:33 -0500

    I love food and Angular

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,11 +2,11 @@ let FoodApp = angular.module('FoodApp', []);
 
 FoodApp.controller('FoodController', function(){
     let vm = this;
-    vm.FoodsILike =[];
+    vm.foodsILike =[];
 
     vm.addFood = function(){
         console.log('in addFood', vm.foodIn);
-        vm.FoodsILike.push(vm.FoodIn);
-        console.log('foods so far:' , vm.FoodsILike);   
+        vm.foodsILike.push(vm.foodIn);
+        console.log('foods so far:' , vm.foodsILike);   
     }
 });
